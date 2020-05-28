fb389eae2a3adf767d4bd5948a59f595d0c3fb38
Kenji <kenji.au9@gmail.com>
2020-05-27 21:12:43 -0600

    Added in Devise Views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_233711) do
+ActiveRecord::Schema.define(version: 2020_05_28_025334) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
